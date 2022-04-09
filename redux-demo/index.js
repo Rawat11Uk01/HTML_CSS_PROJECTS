100372350b6c166ad70b7406d133ab9ad5bacf89
Rawat11Uk01 <kashishrawat308@gmail.com>
2022-04-09 07:33:47 +0530

    first commit

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,30 +1,65 @@
-const redux = require('redux');
+const redux = require("redux");
+const combineReducers = redux.combineReducers;
+const buy_cakes = "buy_cakes";
+const buy_cookies = "buy_cookies";
 
+const buyCake = () => {
+  return {
+    type: buy_cakes,
+    info: "its about cake",
+  };
+};
 
+const buyCookies = () => {
+  return {
+    type: buy_cookies,
+  };
+};
 
-const buy_cakes = 'buy_cakes';
+const initialstateCakestate = {
+  numOfCakes: 10,
+};
 
-const buyCake = ()=>{
-    return{
-        type: buy_cakes,
-        info: "its about cake"
-    }
-}
+const reducerF = (state = initialstateCakestate, action) => {
+  switch (action.type) {
+    case buy_cakes:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
 
-const initialstate = {
-    numOfCakes : 10,
-}
+    default:
+      return state;
+  }
+};
+const initialstateCookiestate = {
+  numOfCookiess: 10,
+};
 
-const reducerF = (state=initialstate,action)=>{
-    switch(action.type){
-        case buy_cakes : return{
-            ...state,
-            numOfCakes: state.numOfCakes -1,
-        }
-        default: return state;
-    }
-    
-}
-const store = redux.createStore(reducerF);
-store.subscribe(()=>{console.log('initialstate', store.getState())});
-store.dispatch(buyCake())
+const cookieReducer = (state = initialstateCookiestate, action) => {
+  switch (action.type){
+    case buy_cookies:
+      return {
+        ...state,
+        numOfCookiess: state.numOfCookiess + 1,
+      };
+      default:
+        return state;
+  }
+};
+const rootReducer = combineReducers({
+  cake: reducerF,
+  cookie: cookieReducer,
+});
+const store = redux.createStore(rootReducer);
+const unsbscribe = store.subscribe(() => {
+  console.log("initialstate", store.getState());
+});
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCookies());
+store.dispatch(buyCookies());
+store.dispatch(buyCookies());
+
+unsbscribe();
