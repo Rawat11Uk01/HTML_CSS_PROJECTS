a2935c2ca09dd9293c52e05cf76c25e65afc3a84
Rawat11Uk01 <kashishrawat308@gmail.com>
2022-04-16 16:51:31 +0530

    first commit

--- practice/practice.js
+++ practice/practice.js
@@ -4,10 +4,19 @@
 //     console.log(total=total+i)
 //     i++;
 // }
+// let num = 10;
 
 
-let num = 10;
+// let total = num*(num+1)/2;
+// console.log(total)
 
 
-let total = num*(num+1)/2;
-console.log(total)
+
+let total = 0
+let i = 11;
+do {
+    i++;
+} while (
+    i < 10
+)
+console.log(i);
