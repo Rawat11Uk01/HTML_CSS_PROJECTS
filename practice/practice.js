bd0d956eaa54ed9cef798f148f3018de126dc30b
Rawat11Uk01 <kashishrawat308@gmail.com>
2022-04-17 04:38:55 +0530

    first commit

--- practice/practice.js
+++ practice/practice.js
@@ -30,3 +30,86 @@
 // console.log(newArray===array1);
 // console.log(newArray);
 
+
+// let fruit = ["apple", 'banna', 'mango'];
+// let array2 = ['ok'];
+// for(let i=0; i<fruit.length; i++){
+//     array2.push(fruit[i]);
+// }
+// console.log(array2);
+
+// let fruit = ["apple", 'banna', 'mango'];
+// let i = 0;
+// while(i<fruit.length){
+//     console.log(fruit[i])
+//     i++;
+// }
+
+// let fruit = ["apple", 'banna', 'mango'];
+// for(let arr of fruit){
+//     console.log(arr);
+// }
+
+// let fruit = ["apple", 'banna', 'mango'];
+
+// const persons = {
+// name: 'rawat',
+// age: 12,
+// list: ['ok','notOk','yesOk']
+// }
+
+// for(let person in persons){
+// console.log(persons[person])
+// }
+
+// console.log(Object.keys(persons));
+// for(let key of Object.keys(persons)){
+//     console.log(persons[key])
+// }
+
+// const key1 = 'objectkey1';
+// const key2 = 'objectkey2';
+
+// const value1 = 'myvalue1';
+// const value2 = 'myvalue2';
+
+// const obj = {};
+// obj[key1]= value1;
+// obj[key2]=value2;
+// console.log(obj);
+
+
+// const newObj = {...'abcdefghijklmnopq1rtuvswxyz'}
+// console.log(newObj);
+
+// const persons = {
+// name: 'rawat',
+// age: 12,
+// list: ['ok','notOk','yesOk']
+// }
+
+// const {name: ok,age,list}=persons;
+
+// console.log(ok)
+
+// const users = [
+//     {userId:1, name:'kashish',age:12},
+//     {userId:2, name:'rawat',age:16},
+//     {userId:3, name:'kittu',age:18},
+// ]
+
+// const [{name}] = users;
+// console.log(name)
+
+// const numbers = [4,2,5,8];
+
+// console.log(numbers.forEach(function(a,b){console.log(a,b)}))
+
+// const users = [{ name: 'kashish', age: 23 }, { name: 'rawat', age: 32 }, { name: 'negi', age: 23 }, { name: 'bisht', age: 23 }];
+
+// console.log(users);
+
+
+
+// const nubers = [1,2,3,45,3,544,2323,2333,2443,5334];
+// console.log(nubers.filter((a)=> a%2!==0))
