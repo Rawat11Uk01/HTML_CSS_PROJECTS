295a20c06641c77a89d663693182df2ab99cd929
Rawat11Uk01 <kashishrawat308@gmail.com>
2022-04-16 16:21:21 +0530

    first commit

--- practice/practice.js
+++ practice/practice.js
@@ -1,2 +1,13 @@
-const useGuess = prompt('guess a no');
-console.log(useGuess);
+// let total= 0;
+// let i = 0;
+// while(i<=10){
+//     console.log(total=total+i)
+//     i++;
+// }
+
+
+let num = 10;
+
+
+let total = num*(num+1)/2;
+console.log(total)
