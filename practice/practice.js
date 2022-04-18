cf5e089ba95fc2944ea10d0f5c1ad365b9f55d28
Rawat11Uk01 <kashishrawat308@gmail.com>
2022-04-18 10:25:54 +0530

    first commit

--- practice/practice.js
+++ practice/practice.js
@@ -424,3 +424,12 @@
 // const newStr = str.split('');
 
 // console.log(newStr.reverse().join(''));
+
+// const str = 'I LOVE KITTU';
+
+
+// let newStr = '';
+// for(let i= str.length-1; i>=0; i--){
+//     newStr+=str[i];
+// }
+// console.log(newStr)
