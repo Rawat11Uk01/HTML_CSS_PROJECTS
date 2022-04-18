6c2e332d91d79df106a582eaaa736ea2d7ca072c
Rawat11Uk01 <kashishrawat308@gmail.com>
2022-04-18 05:37:08 +0530

    first commit

--- practice/practice.js
+++ practice/practice.js
@@ -189,31 +189,31 @@
 // conso
 
 
-const users = [
-    { id: '88f24bea-3825-4237-a0d1-efb6b92d37a4', firstName: 'Sam', lastName: 'Hughes' },
-    { id: '2a35032d-e02b-4508-b3b5-6393aff75a53', firstName: 'Terri', lastName: 'Bishop' },
-    { id: '7f053852-7440-4e44-838c-ddac24611050', firstName: 'Jar', lastName: 'Burke' },
-    { id: 'd456e3af-596a-4224-b1dc-dd990a34c9cf', firstName: 'Julio', lastName: 'Miller' },
-    { id: '58a1e37b-4b15-47c1-b95b-11fe016f7b64', firstName: 'Chester', lastName: 'Flores' },
-    { id: 'b4a306cb-8b95-4f85-b9f8-434dbe010985', firstName: 'Madison', lastName: 'Marshall' },
-    { id: '6ee904be-e3b0-41c9-b7a2-5a0233c38e4c', firstName: 'Ava', lastName: 'Pena' },
-    { id: '7f0ce45a-bdca-4067-968b-d908e79276ce', firstName: 'Gabriella', lastName: 'Steward' },
-    { id: '9e525c2d-6fcd-4d88-9ac4-a44eaf3a43e6', firstName: 'Charles', lastName: 'Campbell' },
-    { id: 'e789565f-fa5a-4d5e-8f6c-dd126cf995be', firstName: 'Madison', lastName: 'Lambert' },
-];
+// const users = [
+//     { id: '88f24bea-3825-4237-a0d1-efb6b92d37a4', firstName: 'Sam', lastName: 'Hughes' },
+//     { id: '2a35032d-e02b-4508-b3b5-6393aff75a53', firstName: 'Terri', lastName: 'Bishop' },
+//     { id: '7f053852-7440-4e44-838c-ddac24611050', firstName: 'Jar', lastName: 'Burke' },
+//     { id: 'd456e3af-596a-4224-b1dc-dd990a34c9cf', firstName: 'Julio', lastName: 'Miller' },
+//     { id: '58a1e37b-4b15-47c1-b95b-11fe016f7b64', firstName: 'Chester', lastName: 'Flores' },
+//     { id: 'b4a306cb-8b95-4f85-b9f8-434dbe010985', firstName: 'Madison', lastName: 'Marshall' },
+//     { id: '6ee904be-e3b0-41c9-b7a2-5a0233c38e4c', firstName: 'Ava', lastName: 'Pena' },
+//     { id: '7f0ce45a-bdca-4067-968b-d908e79276ce', firstName: 'Gabriella', lastName: 'Steward' },
+//     { id: '9e525c2d-6fcd-4d88-9ac4-a44eaf3a43e6', firstName: 'Charles', lastName: 'Campbell' },
+//     { id: 'e789565f-fa5a-4d5e-8f6c-dd126cf995be', firstName: 'Madison', lastName: 'Lambert' },
+// ];
 
-const comments = [
-    { userId: '88f24bea-3825-4237-a0d1-efb6b92d37a4', text: 'Great Job!' },
-    { userId: '7f053852-7440-4e44-838c-ddac24611050', text: 'Well done, I think I understand now!' },
-    { userId: 'e789565f-fa5a-4d5e-8f6c-dd126cf995be', text: 'How do you do that? 😲' },
-    { userId: '7f053852-7440-4e44-838c-ddac24611050', text: 'OK great thanks' },
-    { userId: 'b4a306cb-8b95-4f85-b9f8-434dbe010985', text: 'Cool, thanks!' },
-    { userId: '9e525c2d-6fcd-4d88-9ac4-a44eaf3a43e6', text: 'Nice one 😉' },
-    { userId: '6ee904be-e3b0-41c9-b7a2-5a0233c38e4c', text: 'Got it.' },
-    { userId: '9e525c2d-6fcd-4d88-9ac4-a44eaf3a43e6', text: 'Thanks!' },
-    { userId: '58a1e37b-4b15-47c1-b95b-11fe016f7b64', text: 'Cool 😀' },
-    { userId: '6ee904be-e3b0-41c9-b7a2-5a0233c38e4c', text: 'Great stuff!' },
-];
+// const comments = [
+//     { userId: '88f24bea-3825-4237-a0d1-efb6b92d37a4', text: 'Great Job!' },
+//     { userId: '7f053852-7440-4e44-838c-ddac24611050', text: 'Well done, I think I understand now!' },
+//     { userId: 'e789565f-fa5a-4d5e-8f6c-dd126cf995be', text: 'How do you do that? 😲' },
+//     { userId: '7f053852-7440-4e44-838c-ddac24611050', text: 'OK great thanks' },
+//     { userId: 'b4a306cb-8b95-4f85-b9f8-434dbe010985', text: 'Cool, thanks!' },
+//     { userId: '9e525c2d-6fcd-4d88-9ac4-a44eaf3a43e6', text: 'Nice one 😉' },
+//     { userId: '6ee904be-e3b0-41c9-b7a2-5a0233c38e4c', text: 'Got it.' },
+//     { userId: '9e525c2d-6fcd-4d88-9ac4-a44eaf3a43e6', text: 'Thanks!' },
+//     { userId: '58a1e37b-4b15-47c1-b95b-11fe016f7b64', text: 'Cool 😀' },
+//     { userId: '6ee904be-e3b0-41c9-b7a2-5a0233c38e4c', text: 'Great stuff!' },
+// ];
 // const firstName = users.map((a)=> a.firstName);
 // const lastName = users.map((a)=> a.lastName);
 // console.log(firstName)
@@ -221,5 +221,51 @@ const comments = [
 // console.log(comments.map((s)=>{const {firstName,lastName} = users.find((a)=> a.id === s.userId);
 // return {...s, firstName:firstName, lastName:lastName}}));
 
-console.log(comments.map((s) => { const { firstName, lastName } = users.find((a) => a.id === s.userId);
-return {...s,firstName,lastName} }))
+// console.log(comments.map((s) => { const { firstName, lastName } = users.find((a) => a.id === s.userId);
+// return {...s,firstName,lastName} }))
+// console.log(users.filter((s)=> s.id !== comments.userId))
+
+
+const users = [{
+    id: "e0aafb31-8ac6-48d4-99ee-12af8ddcbdae",
+    email: "tcasaletto0@soundcloud.com",
+    ip_address: "53.215.156.73"
+}, {
+    id: "a7b0221d-5228-4992-a964-b5f3e3d7a022",
+    email: "nmoffett1@paginegialle.it",
+    ip_address: "34.81.219.4"
+}, {
+    id: "1019b52c-db9d-45a2-975f-404455925a6a",
+    email: "rcrampin2@yale.edu",
+    ip_address: "65.181.114.151"
+}, {
+    id: "e8637c3b-645d-4742-a463-d2006742606f",
+    email: "ddreinan3@smugmug.com",
+    ip_address: "136.171.217.116"
+}, {
+    id: "a20803f2-c372-4f39-97b1-d8e948151c8c",
+    email: "gdorkin4@taobao.com",
+    ip_address: "220.228.4.97"
+}, {
+    id: "63f8e789-009d-4e7b-ad6b-cc8c8a1d1248",
+    email: "vhucquart5@typepad.com",
+    ip_address: "101.212.0.49"
+}, {
+    id: "74120248-03b5-4888-84c0-d1a459c6273b",
+    email: "vsircombe6@marriott.com",
+    ip_address: "28.180.205.167"
+}, {
+    id: "412a0669-7171-48b4-95eb-e898993ec1dc",
+    email: "kdesouza7@t.co",
+    ip_address: "61.44.195.62"
+}, {
+    id: "6cd04ecc-8316-4542-9faa-b8438e82b4ad",
+    email: "sbehnke8@booking.com",
+    ip_address: "1.176.238.27"
+}, {
+    id: "cfdf5009-84d3-4599-88c0-048c4fcae2ed",
+    email: "clindsell9@nymag.com",
+    ip_address: "44.37.119.76"
+}];
+
+console.log(users.filter(({ip_address})=>ip_address.split('.')[0] < 128 ))
